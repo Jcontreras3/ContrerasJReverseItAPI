c279968f6d430f5b3a6bf62ace047df932d8aeac
Jovann Contreras <jcontreras@codestack.co>
2022-10-27 12:59:12 -0700

    github

---
+++ Controllers/ReverseController.cs
@@ -0,0 +1,32 @@
+//Jovann Contreras
+//Date 10/27/2022
+//Peer reviewed by:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReverseIt.Models;
+
+namespace ContrerasJReverseItAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReverseController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Reverse")]
+
+        public string ReverseItChallenge(ReverseItModel item)
+        {
+              string reverseString = "";
+            for (int i = item.revStr.Length - 1; i >= 0; i--)
+            {
+                reverseString = reverseString + item.revStr.Substring(i, 1);
+            }
+
+            return reverseString;
+        }
+    }
+}
---
+++ Models/ReverseItModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseIt.Models
+{
+    public class ReverseItModel
+    {
+        public string revStr {get; set;}
+    }
+}
